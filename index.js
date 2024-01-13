f4f6b0b0b1363cd6367497070578f3fc80702fcd
typeroo <138537464+typeroo@users.noreply.github.com>
2024-01-13 13:15:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ var random_Number;
 random_Number=Math.floor(Math.random()*6)+1;
 random_Number1=Math.floor(Math.random()*6)+1;
 
-document.querySelector(".img1").src="./images/dice"+random_Number+".png";
-document.querySelector(".img2").src="./images/dice"+random_Number1+".png";
+document.querySelector(".img1").src="./dice"+random_Number+".png";
+document.querySelector(".img2").src="./dice"+random_Number1+".png";
 if(random_Number<random_Number1){
     document.querySelector("h1").textContent="Player 2 wins!!";
 } 
